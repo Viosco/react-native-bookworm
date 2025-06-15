eaf1c6e706b05629c26b1e9283a2604f3ac621d4
Viosco <showersviome@gmail.com>
2025-06-16 00:52:47 +0200

    updated deleting book image fifth time

--- src/lib/cloudinary.js
+++ src/lib/cloudinary.js
@@ -1,12 +1,25 @@
 import { v2 } from 'cloudinary';
-//import dotenv from 'dotenv';
-//dotenv.config(); // Load env vars
+import dotenv from 'dotenv';
 
-export default cloudinary = v2.config({
+dotenv.config(); // Load env vars
+
+const cloudinary = v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+export default cloudinary;
+
+// import {v2} from 'cloudinary';
+
+// const cloudinary = v2.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// export default cloudinary;
 
 
 // const uploadImage = async (image) => {
